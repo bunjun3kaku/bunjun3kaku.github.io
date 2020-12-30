a6be0819ccadc9bdec9b280abb7e689c8fbf45e0
bunjun3kaku <149thbunjunsankaku@gmail.com>
2020-12-30 15:08:26 +0900

    renew file 12301508

--- form.js
+++ form.js
@@ -1,10 +1,12 @@
   document.getElementById("form-button").onclick = function() {
     var answer = document.getElementById("form-answer").value;
-    if (answer == 30) {
-      var result = window.confirm("あなたは正解しました。\nステージ2に進みますか？");
+    if (answer == "かいせいぶんかさい") {
+      var result = window.confirm("あなたは正解しました。");
       if (result) {
         window.open('index.html', '_blank')
       }
+    }else if (answer == "でこれーしょん") {
+      window.open('https://www.youtube.com', '_blank')
     }else {
       alert("不正解");
     };
